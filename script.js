8cff7071e8cc0bf0c47865b03d94cf75c846b78d
AbnerAbreu <abnerdrift@gmail>
2019-07-19 17:46:34 -0300

    Adicionar empréstimo bancário

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-let dia1 = "Domingo" ;
-let dia2 = "Segunda-Feira";
-let dia3 = "Terça-Feira" ;
-let dia4 = "Quarta-Feira";
-let dia5 = "Quinta-Fiera";
-let dia6 = "Sexta-Feira";
-let dia7 = "Sabado" ;
- 
-let numero = Number(prompt("Digite um número de 1 a 7"));
+if (idade >= 25 && idade <= 60) {
+    let salario = Number(prompt("Digite seu salário"));
+    if (salario >= 2000) {
+        let emprestimo = Number(prompt("Digite o valor desejado para empréstimo"));
+        if (emprestimo < 10 * salario) {
+            console.log("Emprestimo Aprovado");
 
-if(numero == 1 ){
-    console.log(`O dia ${numero} é ${dia1}`);
-}
+        } else {
+            console.log("Empréstimo negado por exeder valor");
+        }
 
-else if(numero == 2){
-    console.log(`O dia ${numero} é ${dia2}`)
-}
+    } else {
+        console.log("Operação reprovada por salário");
 
-else if(numero == 3){
-    console.log(`O dia ${numero} é ${dia3}`)
-}
-
-else if(numero == 4){
-    console.log(`O dia ${numero} é ${dia4}`)
-}
-
-else if(numero == 5){
-    console.log(`O dia ${numero} é ${dia5}`)
-}
-
-else if(numero == 6){
-    console.log(`O dia ${numero} é ${dia6}`)
-}
-
-else if(numero == 7){
-    console.log(`O dia ${numero} é ${dia7}`)
+    }
+} else {
+    console.log("Recusado porque não tem idade pra isso")
 }
